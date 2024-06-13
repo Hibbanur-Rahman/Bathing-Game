c5ccf4f551ca54b104a9cdcff3f862db4604083d
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-06-13 12:08:51 +0530

    adding the home ui setup

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,21 @@
-const Home=()=>{
-    return (
-        <>
-            <h1>Home</h1>
-        </>
-    )
-}
+const Home = () => {
+  return (
+    <div className="home row m-0 p-0 justify-content-center align-items-center">
+      <div className="start-screen row m-0 p-0 justify-content-center align-items-center">
+        <h1 className="m-0 p-0 text-center">Taking A Bath</h1>
+        <div className="col-10 m-0 p-4 d-flex rounded-3 border border-5 border-color" style={{backgroundColor:'rgba(198,223,251,0.50)'}}>
+                <div className="col-6">
+                    <h1 className="m-0 p-0">Help Bala to take Bath</h1>
+                    <div className="card w-auto border border-4">
+                        
+                    </div>
+
+                </div>
+                <div className="col-6"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
