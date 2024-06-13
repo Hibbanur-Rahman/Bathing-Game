1d5482286ed96d6b8f88e1a4aa1fc394ea913171
Meraj <haquedot@gmail.com>
2024-06-13 11:53:34 +0530

    Home ui created

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,34 @@
-const Home=()=>{
+import '../assets/styles/style-Home.css'
+import image1 from '../assets/images/boy-bathtub.svg'
+
+const Home = () => {
     return (
         <>
-            <h1>Home</h1>
+            <div className="home">
+                <div className="container">
+                    <h1 className="text-center">Taking  A Bath</h1>
+                    <div className="play-area">
+                        <div className="d-flex">
+                            <div className="row">
+                                <div className="col-lg-5">
+                                    <h1>Help Bala to<br />take Bath</h1>
+                                    <div className="d-block">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="check" />
+                                        </div>
+                                        <button type="button" className='skip-btn'>Skip to<br />Checklist</button>
+                                    </div>
+                                </div>
+                                <div className="col-lg-7">
+                                    <div className="boy-bathtub w-100">
+                                        <img src={image1} alt="boy-bathtub" className='w-100' />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
